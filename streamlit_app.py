d0e6e376a65637c36fbe3ab32f349ddae445093c
abedbsatt <95166572+abedbsatt@users.noreply.github.com>
2023-10-10 17:45:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,39 @@
-#Import packages necessary to work with csv file and streamlit
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+# Load the data
+@st.cache
+def load_data():
+    data = pd.read_csv('Top_1000_IMDb_movies_New_version.csv')
+    # Clean the 'Votes' column and convert it to integer
+    data['Votes'] = data['Votes'].str.replace(',', '').astype(int)
+    # Convert 'Year of Release' to integer
+    data['Year of Release'] = pd.to_numeric(data['Year of Release'], errors='coerce')
+    return data
 
-#Importing the CSV file into Python
-imdb_movies = "Top_1000_IMDb_movies_New_version.csv"
-df = pd.read_csv(imdb_movies)
+data = load_data()
 
-# Data cleaning
-data['Votes'] = data['Votes'].str.replace(',', '').astype(int)
-data['Year of Release'] = pd.to_numeric(data['Year of Release'], errors='coerce')
-return data
-
-df = load_data()
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-
-# Title and Introduction
+# Title
 st.title("IMDb Top 1000 Movies Exploration")
+
+# Introduction
 st.write("""
 Explore the top 1000 movies according to IMDb. 
 Use the side panel to interact with the visualizations below!
 """)
 
-# Sidebar for interactive features
+# Sidebar
 st.sidebar.header('Interactive Features')
 
 # Visualization 1: Histogram of IMDb Ratings
 st.subheader("Distribution of IMDb Ratings")
 
 # Interactive Feature 1: Filter based on year
-year_to_filter = st.sidebar.slider('Year of release', int(df['Year of Release'].min()), int(df['Year of Release'].max()), (int(df['Year of Release'].min()), int(df['Year of Release'].max())))
+year_to_filter = st.sidebar.slider('Year of release', int(data['Year of Release'].min()), int(data['Year of Release'].max()), (int(data['Year of Release'].min()), int(data['Year of Release'].max())))
 
 # Filter the data
-filtered_data = df[(df['Year of Release'] >= year_to_filter[0]) & (df['Year of Release'] <= year_to_filter[1])]
+filtered_data = data[(data['Year of Release'] >= year_to_filter[0]) & (data['Year of Release'] <= year_to_filter[1])]
 
 # Plot using Plotly Express
 fig1 = px.histogram(filtered_data, x='Movie Rating', nbins=30, title='Distribution of IMDb Ratings')
@@ -47,10 +45,10 @@ st.plotly_chart(fig1)
 st.subheader("IMDb Rating vs Metascore")
 
 # Interactive Feature 2: Select a range of IMDb ratings to visualize
-rating_range = st.sidebar.slider('IMDb Rating range', float(df['Movie Rating'].min()), float(df['Movie Rating'].max()), (float(df['Movie Rating'].min()), float(df['Movie Rating'].max())))
+rating_range = st.sidebar.slider('IMDb Rating range', float(data['Movie Rating'].min()), float(data['Movie Rating'].max()), (float(data['Movie Rating'].min()), float(data['Movie Rating'].max())))
 
 # Filter the data
-filtered_data_2 = df[(df['Movie Rating'] >= rating_range[0]) & (df['Movie Rating'] <= rating_range[1])]
+filtered_data_2 = data[(data['Movie Rating'] >= rating_range[0]) & (data['Movie Rating'] <= rating_range[1])]
 
 # Plot using Plotly Express
 fig2 = px.scatter(filtered_data_2, x='Movie Rating', y='Metascore of movie', title='IMDb Rating vs Metascore')
