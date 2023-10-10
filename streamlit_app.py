a5475e9ebfef0bbb3cc9b915f7f60542e0f8c828
abedbsatt <95166572+abedbsatt@users.noreply.github.com>
2023-10-10 17:42:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,58 +1,59 @@
 #Import packages necessary to work with csv file and streamlit
 
-import pandas as pd 
-import plotly.express as px
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 
-#Importing the csv file into Python
+#Importing the CSV file into Python
 imdb_movies = "Top_1000_IMDb_movies_New_version.csv"
 df = pd.read_csv(imdb_movies)
+
+# Data cleaning
+    data['Votes'] = data['Votes'].str.replace(',', '').astype(int)
+    data['Year of Release'] = pd.to_numeric(data['Year of Release'], errors='coerce')
+    return data
+
+df = load_data()
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
+# Title and Introduction
+st.title("IMDb Top 1000 Movies Exploration")
+st.write("""
+Explore the top 1000 movies according to IMDb. 
+Use the side panel to interact with the visualizations below!
+""")
 
+# Sidebar for interactive features
+st.sidebar.header('Interactive Features')
 
-st.title('IMDB Top 1000 Movies App!')
-st.write('This app will display some interactive visualizations regarding the Top 1000 IMDB Movies!')
-st.write(df)
+# Visualization 1: Histogram of IMDb Ratings
+st.subheader("Distribution of IMDb Ratings")
 
-average_rating_per_year = df.groupby('Year of Release')['Movie Rating'].mean().reset_index()
-fig2 = px.line(average_rating_per_year, x='Year of Release', y='Movie Rating', title='Average Movie Rating Over the Years')
-st.plotly_chart(fig2)
+# Interactive Feature 1: Filter based on year
+year_to_filter = st.sidebar.slider('Year of release', int(df['Year of Release'].min()), int(df['Year of Release'].max()), (int(df['Year of Release'].min()), int(df['Year of Release'].max())))
 
+# Filter the data
+filtered_data = df[(df['Year of Release'] >= year_to_filter[0]) & (df['Year of Release'] <= year_to_filter[1])]
 
-
-fig3 = px.histogram(df, x='Movie Rating', title='Distribution of Movie Ratings', labels={'Movie Rating': 'Movie Rating'}, nbins=20)
-st.plotly_chart(fig3)
-
-
-#Sidebar
-
-st.sidebar.header('User Input Parameters')
-year_range = st.sidebar.slider('Year of Release Range', 1980, 2022, (1990, 2020))
-rating_range = st.sidebar.slider('Movie Rating Range', 0.0, 10.0, (5.0, 9.0))
-
-# Filter the data according to user inputs
-filtered_data = df[(df['Year of Release'].between(year_range[0], year_range[1])) & (df['Movie Rating'].between(rating_range[0], rating_range[1]))]
-st.write(filtered_data)
-
-def load_data():
-    df['Year of Release'] = pd.to_numeric(df['Year of Release'], errors='coerce')
-    df['Movie Rating'] = pd.to_numeric(df['Movie Rating'], errors='coerce')
-    df.dropna(subset=['Year of Release', 'Movie Rating'], inplace=True)
-    return df
-
-
-#Scatter Plot Visualization
-
-st.subheader('Scatter Plot: Movie Rating vs Metascore')
-fig1 = px.scatter(filtered_data, x='Movie Rating', y='Metascore of movie', labels={'Movie Rating': 'Movie Rating', 'Metascore of movie': 'Metascore'}, hover_data=['Movie Name', 'Year of Release'])
+# Plot using Plotly Express
+fig1 = px.histogram(filtered_data, x='Movie Rating', nbins=30, title='Distribution of IMDb Ratings')
+fig1.update_xaxes(title='IMDb Rating')
+fig1.update_yaxes(title='Frequency')
 st.plotly_chart(fig1)
 
-#Line Plot Visualization
-st.subheader('Line Plot: Average Movie Ratings Over Years')
-average_rating_per_year = filtered_data.groupby('Year of Release')['Movie Rating'].mean().reset_index()
-fig2 = px.line(average_rating_per_year, x='Year of Release', y='Movie Rating', title='Average Movie Rating Over the Years')
-st.plotly_chart(fig2)
+# Visualization 2: Scatter plot of IMDb Rating vs Metascore
+st.subheader("IMDb Rating vs Metascore")
 
+# Interactive Feature 2: Select a range of IMDb ratings to visualize
+rating_range = st.sidebar.slider('IMDb Rating range', float(df['Movie Rating'].min()), float(df['Movie Rating'].max()), (float(df['Movie Rating'].min()), float(df['Movie Rating'].max())))
+
+# Filter the data
+filtered_data_2 = df[(df['Movie Rating'] >= rating_range[0]) & (df['Movie Rating'] <= rating_range[1])]
+
+# Plot using Plotly Express
+fig2 = px.scatter(filtered_data_2, x='Movie Rating', y='Metascore of movie', title='IMDb Rating vs Metascore')
+fig2.update_xaxes(title='IMDb Rating')
+fig2.update_yaxes(title='Metascore')
+st.plotly_chart(fig2)
